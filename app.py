05d01cad89236b49d326616284285356cac5e456
Sanskaragrawal2107 <sanskar21072005@gmail.com>
2024-09-27 16:23:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import joblib
 
 app = Flask(__name__)
 
-# Load the model and label encoders
 model = joblib.load('crop_model.pkl')
 state_label_encoder = joblib.load('state_label_encoder.pkl')
 crop_label_encoder = joblib.load('crop_label_encoder.pkl')
@@ -16,27 +15,27 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get data from the form and convert to float
+       
         int_features = [float(request.form.get(feature)) for feature in ['N_SOIL', 'P_SOIL', 'K_SOIL', 'TEMPERATURE', 'HUMIDITY', 'ph', 'RAINFALL', 'CROP_PRICE']]
     except ValueError as e:
         return render_template('index.html', prediction_text=f'Error: Please enter valid numerical values. {str(e)}')
 
-    # Get the STATE input
+  
     STATE = request.form.get('STATE')
 
-    # Encode 'STATE'
+  
     try:
         STATE_encoded = state_label_encoder.transform([STATE])
     except ValueError:
         return render_template('index.html', prediction_text='Error: Invalid state name. Please ensure it is correct.')
 
-    # Combine all features
+  
     final_features = np.array([[*int_features, STATE_encoded[0]]])
     
-    # Make the prediction
+   
     prediction = model.predict(final_features)
     
-    # Decode the predicted crop
+   
     predicted_crop = crop_label_encoder.inverse_transform(prediction)
 
     return render_template('index.html', prediction_text=f'The predicted crop is: {predicted_crop[0]}')
